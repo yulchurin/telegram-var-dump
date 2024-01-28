1076f80fd7fb8f5916a2dc6aa6cbcaa7b4353305
Tayfur Yulchurin <yulchurin@gmail.com>
2024-01-28 18:26:27 +0500

    fix path

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,7 +6,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function register()
     {
-        $configPath = __DIR__ . '/../config/tg-var-dump.php';
+        $configPath = __DIR__ . '/config/tg-var-dump.php';
 
         $this->mergeConfigFrom($configPath, 'tg-var-dump');
     }
